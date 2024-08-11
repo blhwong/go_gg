88de130f29e691342c661f646486b32e42ac3191
Brandon Wong <b.lh.wong@gmail.com>
2024-08-11 14:47:31 -0700

    Move in memory db service

--- db/db.go
+++ db/db.go
@@ -2,8 +2,6 @@ package db
 
 import (
 	"gg/client/startgg"
-	"strconv"
-	"strings"
 )
 
 type DBServiceInterface interface {
@@ -13,51 +11,4 @@ type DBServiceInterface interface {
 	SetIsCharactersLoaded(slug string)
 	AddSets(slug string, setMapping *map[string]string)
 	GetSets(slug string) *map[string]string
-}
-
-type InMemoryDBService struct {
-	storage map[string]string
-}
-
-func NewInMemoryDBService() *InMemoryDBService {
-	return &InMemoryDBService{storage: make(map[string]string, 0)}
-}
-
-func (db *InMemoryDBService) IsCharactersLoaded(slug string) bool {
-	return db.storage[slug+"_is_character_loaded"] == "1"
-}
-
-func (db *InMemoryDBService) GetCharacterName(key int, slug string) string {
-	return db.storage[slug+"_"+strconv.Itoa(key)]
-}
-
-func (db *InMemoryDBService) AddCharacters(characters []startgg.Character, slug string) {
-	for _, character := range characters {
-		db.storage[slug+"_"+strconv.Itoa(character.Id)] = character.Name
-	}
-}
-
-func (db *InMemoryDBService) SetIsCharactersLoaded(slug string) {
-	db.storage[slug+"_is_character_loaded"] = "1"
-}
-
-func (db *InMemoryDBService) AddSets(slug string, setMapping *map[string]string) {
-	for setId, s := range *setMapping {
-		db.AddSet(slug, setId, s)
-	}
-}
-
-func (db *InMemoryDBService) AddSet(slug string, setId string, set string) {
-	db.storage[slug+"_"+setId] = set
-}
-
-func (db *InMemoryDBService) GetSets(slug string) *map[string]string {
-	setMapping := make(map[string]string, 0)
-	for key, set := range db.storage {
-		parts := strings.Split(key, "_")
-		if parts[0] == slug {
-			setMapping[parts[1]] = set
-		}
-	}
-	return &setMapping
 }
--- service/service_test.go
+++ service/service_test.go
@@ -2,13 +2,15 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"gg/client/startgg"
-	"gg/db"
 	"gg/domain"
 	"gg/mapper"
 	"log"
 	"os"
 	"slices"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -46,8 +48,56 @@ func (f *FakeFileReaderWriter) WriteString(filename, data string) {
 var fakeStartGGClient startgg.ClientInterface = &FakeStartGGClient{}
 var fakeFileReaderWriter FileInterface = &FakeFileReaderWriter{}
 
+type InMemoryDBService struct {
+	storage map[string]string
+}
+
+func NewInMemoryDBService() *InMemoryDBService {
+	return &InMemoryDBService{storage: make(map[string]string, 0)}
+}
+
+func (db *InMemoryDBService) IsCharactersLoaded(slug string) bool {
+	fmt.Println("Inside is characters loaded")
+	return db.storage[slug+"_is_character_loaded"] == "1"
+}
+
+func (db *InMemoryDBService) GetCharacterName(key int, slug string) string {
+	return db.storage[slug+"_"+strconv.Itoa(key)]
+}
+
+func (db *InMemoryDBService) AddCharacters(characters []startgg.Character, slug string) {
+	for _, character := range characters {
+		db.storage[slug+"_"+strconv.Itoa(character.Id)] = character.Name
+	}
+}
+
+func (db *InMemoryDBService) SetIsCharactersLoaded(slug string) {
+	db.storage[slug+"_is_character_loaded"] = "1"
+}
+
+func (db *InMemoryDBService) AddSets(slug string, setMapping *map[string]string) {
+	for setId, s := range *setMapping {
+		db.AddSet(slug, setId, s)
+	}
+}
+
+func (db *InMemoryDBService) AddSet(slug string, setId string, set string) {
+	db.storage[slug+"_"+setId] = set
+}
+
+func (db *InMemoryDBService) GetSets(slug string) *map[string]string {
+	setMapping := make(map[string]string, 0)
+	for key, set := range db.storage {
+		parts := strings.Split(key, "_")
+		if parts[0] == slug {
+			setMapping[parts[1]] = set
+		}
+	}
+	return &setMapping
+}
+
 var service = NewService(
-	db.NewInMemoryDBService(),
+	NewInMemoryDBService(),
 	fakeStartGGClient,
 	fakeFileReaderWriter,
 )
